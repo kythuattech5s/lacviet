4813e859ce1ff26e559c01158a47489a0e7ef9e8
trinh toanh <kythuat.tech5s@gmail.com>
2022-09-28 16:50:34 +0700

    update code

--- packages/vanhenry/manager/src/views/view/table.blade.php
+++ packages/vanhenry/manager/src/views/view/table.blade.php
@@ -1,9 +1,9 @@
 @php
-$has_delete = $tableData->get('has_delete', '') == 1;
-$has_update = $tableData->get('has_update', '') == 1;
-$has_copy = $tableData->get('has_copy', '') == 1;
-$has_trash = $tableData->get('has_trash', '') == 1;
-$has_history = $tableData->get('has_history', '') == 1;
+    $has_delete = $tableData->get('has_delete', '') == 1;
+    $has_update = $tableData->get('has_update', '') == 1;
+    $has_copy = $tableData->get('has_copy', '') == 1;
+    $has_trash = $tableData->get('has_trash', '') == 1;
+    $has_history = $tableData->get('has_history', '') == 1;
 @endphp
 <div class="pagination">
     <span class="total">{{ trans('db::number_record') }}: <strong>{{ $listData->total() }}</strong></span>
@@ -31,26 +31,26 @@ $has_history = $tableData->get('has_history', '') == 1;
         @if (isset($check))
             @if (isset($deleted_now) && $deleted_now->count() > 0)
                 <a class="_vh_action_all btn btn-danger" data-confirm="Bạn có thực sự muốn xóa?"
-                   href="{{ $admincp }}/deleteAll/{{ $tableData->get('table_map', '') }}"
-                   title="{{ trans('db::delete_all') }} {{ $tableData->get('name', '') }}"><i class="fa fa-trash"
-                       aria-hidden="true"></i> {{ trans('db::delete_all') }}</a>
+                    href="{{ $admincp }}/deleteAll/{{ $tableData->get('table_map', '') }}"
+                    title="{{ trans('db::delete_all') }} {{ $tableData->get('name', '') }}"><i class="fa fa-trash"
+                        aria-hidden="true"></i> {{ trans('db::delete_all') }}</a>
             @endif
         @else
             <a class="_vh_action_all btn btn-danger" data-confirm="Bạn có thực sự muốn xóa?"
-               href="{{ $admincp }}/deleteAll/{{ $tableData->get('table_map', '') }}"
-               title="{{ trans('db::delete_all') }} {{ $tableData->get('name', '') }}"><i class="fa fa-trash"
-                   aria-hidden="true"></i> {{ trans('db::delete_all') }}</a>
+                href="{{ $admincp }}/deleteAll/{{ $tableData->get('table_map', '') }}"
+                title="{{ trans('db::delete_all') }} {{ $tableData->get('name', '') }}"><i class="fa fa-trash"
+                    aria-hidden="true"></i> {{ trans('db::delete_all') }}</a>
         @endif
         @php
             $data_actions = $tableData->get('default_action_all', []);
             $data_actions = json_decode($data_actions, true);
         @endphp
         @if ($data_actions !== null && count($data_actions) > 0)
-            @foreach ($data_actions as $action)
+        @foreach ($data_actions as $action)
                 @if ((isset($check) && isset($deleted_now) && $deleted_now->count() > 0) || (isset($action['permis']) && Str::lower($action['permis']) == 'all'))
                     <a class="_vh_action_all btn btn-danger" data-confirm="{{ $action['message'] }}"
-                       href="{{ $admincp }}/{{ $action['href'] }}/{{ $tableData->get('table_map', '') }}"
-                       tite="{{ $action['title'] }}">
+                        href="{{ $admincp }}/{{ $action['href'] }}/{{ $tableData->get('table_map', '') }}"
+                        tite="{{ $action['title'] }}">
                         {!! $action['icon'] !!}
                         {{ $action['title'] }}
                     </a>
@@ -59,10 +59,10 @@ $has_history = $tableData->get('has_history', '') == 1;
         @endif
         @if ($tableData->get('table_parent', '') != '')
             <a href="#" data-toggle="modal" data-target="#addToParent" class="_vh_add_to_parent"
-               title="Thêm vào danh mục cha"><i class="fa fa-puzzle-piece" aria-hidden="true">Thêm vào danh mục cha</i>
+                title="Thêm vào danh mục cha"><i class="fa fa-puzzle-piece" aria-hidden="true">Thêm vào danh mục cha</i>
             </a>
             <a href="#" title="Xóa khỏi danh mục cha" data-toggle="modal" data-target="#addToParent"
-               class="_vh_remove_from_parent"><i class="fa fa-chain-broken" aria-hidden="true">Xóa khỏi danh mục
+                class="_vh_remove_from_parent"><i class="fa fa-chain-broken" aria-hidden="true">Xóa khỏi danh mục
                     cha</i></a>
         @endif
     </div>
@@ -79,9 +79,9 @@ $has_history = $tableData->get('has_history', '') == 1;
                         <th>
                             <div class="squaredTwo">
                                 <input type="checkbox" class="all" value="None"
-                                       id="squaredTwoall{{ @$dataKey ?? '' }}" name="check">
+                                    id="squaredTwoall{{ @$dataKey ?? '' }}" name="check">
                                 <label for="squaredTwoall{{ @$dataKey ?? '' }}"
-                                       data-tab="{{ @$dataKey ?? '' }}"></label>
+                                    data-tab="{{ @$dataKey ?? '' }}"></label>
                             </div>
                         </th>
                     @endif
@@ -116,8 +116,8 @@ $has_history = $tableData->get('has_history', '') == 1;
                             <td data-title="#">
                                 <div class="squaredTwo">
                                     <input type="checkbox" class="one"
-                                           dt-id="{{ FCHelper::ep($itemMain, 'id') }}"
-                                           id="squaredTwo{{ FCHelper::ep($itemMain, 'id') }}" name="check">
+                                        dt-id="{{ FCHelper::ep($itemMain, 'id') }}"
+                                        id="squaredTwo{{ FCHelper::ep($itemMain, 'id') }}" name="check">
                                     <label for="squaredTwo{{ FCHelper::ep($itemMain, 'id') }}"></label>
                                 </div>
                             </td>
@@ -129,54 +129,53 @@ $has_history = $tableData->get('has_history', '') == 1;
                                 $viewView = View::exists($viewView) ? $viewView : 'vh::ctview.base';
                             @endphp
                             @if ($show->hide !== 1)
-                                @include($viewView, ['item' => $show, 'dataItem' => $itemMain])
+                                @include($viewView,array('item'=>$show,'dataItem'=>$itemMain))
                             @endif
                         @endforeach
-                        @if ($has_copy || $has_update || $has_trash || $has_history)
+                        @if ($has_copy || $has_update || $has_trash || $has_history || $has_delete)
                             <td data-title="{{ trans('db::function') }}" style="min-width: 130px;"
                                 class="action">
-                                @if ($tableData->get('table_map', '') == 'pages')
-                                    <a href="/gp/edit-page?id={{ FCHelper::ep($itemMain, 'id') }}" target="_blank"
-                                       class="{{ trans('db::edit') }} tooltipx {{ $tableData->get('table_map', '') }}">
-                                        <i class="fa fa-edit" aria-hidden="true"></i>
-                                        <span class="tooltiptext">Chỉnh sửa trang</span>
+                                @if ($tableData->get('table_map') == 'book_apointments')
+                                    <a href="{{$admincp}}/manage-book-apointments?id={{ $itemMain->id }}" class="tooltipx {{ $tableData->get('table_map', '') }}">
+                                        <i class="fa fa-eye" aria-hidden="true"></i>
+                                        <span class="tooltiptext">Quản lý đăt lịch</span>
                                     </a>
                                 @endif
                                 @isset($itemMain->slug)
                                     <a href="{{ $itemMain->slug }}" target="_blank"
-                                       class="{{ trans('db::edit') }} tooltipx {{ $tableData->get('table_map', '') }}">
+                                        class="{{ trans('db::edit') }} tooltipx {{ $tableData->get('table_map', '') }}">
                                         <i class="fa fa-eye" aria-hidden="true"></i>
                                         <span class="tooltiptext">Xem demo</span>
                                     </a>
                                 @endisset
                                 @if ($has_history)
                                     <a href="{{ $admincp }}/history/{{ $tableData->get('table_map', '') }}/{{ FCHelper::ep($itemMain, 'id') }}?returnurl={{ $urlFull }}"
-                                       class="{{ trans('db::history') }} tooltipx {{ $tableData->get('table_map', '') }}">
+                                        class="{{ trans('db::history') }} tooltipx {{ $tableData->get('table_map', '') }}">
                                         <i class="fa fa-history" aria-hidden="true"></i>
                                         <span class="tooltiptext">Lịch sử thay đổi</span>
                                     </a>
                                 @endif
                                 @if ($has_copy)
                                     <a href="{{ $admincp }}/copy/{{ $tableData->get('table_map', '') }}/{{ FCHelper::ep($itemMain, 'id') }}?returnurl={{ $urlFull }}"
-                                       class="{{ trans('db::edit') }} tooltipx {{ $tableData->get('table_map', '') }}"><i
-                                           class="fa fa-copy" aria-hidden="true"></i>
+                                        class="{{ trans('db::edit') }} tooltipx {{ $tableData->get('table_map', '') }}"><i
+                                            class="fa fa-copy" aria-hidden="true"></i>
                                         <span class="tooltiptext">Copy</span>
                                     </a>
                                 @endif
                                 @if ($has_update)
                                     <a href="{{ $admincp }}/edit/{{ $tableData->get('table_map', '') }}/{{ FCHelper::ep($itemMain, 'id') }}?returnurl={{ $urlFull }}"
-                                       class="{{ trans('db::edit') }} tooltipx {{ $tableData->get('table_map', '') }}"><i
-                                           class="fa fa-pencil" aria-hidden="true"></i>
+                                        class="{{ trans('db::edit') }} tooltipx {{ $tableData->get('table_map', '') }}"><i
+                                            class="fa fa-pencil" aria-hidden="true"></i>
                                         <span class="tooltiptext">Sửa</span>
                                     </a>
                                 @endif
                                 @if ($has_trash)
                                     <a href="{{ $admincp }}/{{ isset($trash) ? 'backtrash' : 'trash' }}/{{ $tableData->get('table_map', '') }}"
-                                       class="_vh_{{ isset($trash) ? 'backtrash' : 'trash' }} tooltipx {{ trans('db::delete') }} {{ $tableData->get('table_map', '') }}"><i
-                                           class="fa fa-{{ isset($trash) ? 'level-up' : 'trash' }}"
-                                           aria-hidden="true"></i>
+                                        class="_vh_{{ isset($trash) ? 'backtrash' : 'trash' }} tooltipx {{ trans('db::delete') }} {{ $tableData->get('table_map', '') }}"><i
+                                            class="fa fa-{{ isset($trash) ? 'level-up' : 'trash' }}"
+                                            aria-hidden="true"></i>
                                         <span
-                                              class="tooltiptext">{{ isset($trash) ? 'Restore' : 'Thùng rác' }}</span>
+                                            class="tooltiptext">{{ isset($trash) ? 'Restore' : 'Thùng rác' }}</span>
                                     </a>
                                 @endif
                                 @include('vh::view.table.action_delete')
