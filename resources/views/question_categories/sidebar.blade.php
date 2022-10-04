71d6605c203cc031d5350280985d345143bf9e56
RONIEJISA <roniejisa@gmail.com>
2022-10-04 10:22:00 +0700

    update code

--- resources/views/question_categories/sidebar.blade.php
+++ resources/views/question_categories/sidebar.blade.php
@@ -1,21 +1,28 @@
 @php
-    $listCate = \App\Models\QuestionCategory::act()->where('parent',0)->with(['child'=>function($q){
-                                                        $q->act();
-                                                    }])->get();
+if (!isset($listCate)) {
+    $listCate = \App\Models\QuestionCategory::act()
+        ->where('parent', 0)
+        ->with([
+            'child' => function ($q) {
+                $q->act();
+            },
+        ])
+        ->get();
+}
 @endphp
 @if (count($listCate) > 0)
     <div class="nav-category">
-        <span class="close-category block lg:hidden absolute top-1 right-1 z-[1] w-5 h-5"></span>
-        <p class="title-category relative after:w-full lg:after:h-1 after:h-[2px] after:bg-[#008EDF] after:block after:mt-1 font-bold text-[#252525] uppercase 2xl:text-[2rem] lg:text-[1.5rem] text-[1rem] mb-4">Danh mục</p>
+        <span class="close-category absolute top-1 right-1 z-[1] block h-5 w-5 lg:hidden"></span>
+        <p class="title-category relative mb-4 text-[1rem] font-bold uppercase text-[#252525] after:mt-1 after:block after:h-[2px] after:w-full after:bg-[#008EDF] lg:text-[1.5rem] lg:after:h-1 2xl:text-[2rem]">Danh mục</p>
         <ul class="nav-service">
             @foreach ($listCate as $itemCate)
                 <li>
-                    <a href="{{Support::show($itemCate,'slug')}}" title="{{Support::show($itemCate,'name')}}" class="block py-2 font-bold text-[#028cde] 2xl:text-[1.375rem] lg:text[1.125rem] text-[0.875rem]">{{Support::show($itemCate,'name')}}</a>
+                    <a href="{{ Support::show($itemCate, 'slug') }}" title="{{ Support::show($itemCate, 'name') }}" class="lg:text[1.125rem] block py-2 text-[0.875rem] font-bold text-[#028cde] 2xl:text-[1.375rem]">{{ Support::show($itemCate, 'name') }}</a>
                     @if (count($itemCate->child) > 0)
-                        <ul class="list-disc ml-8">
+                        <ul class="ml-8 list-disc">
                             @foreach ($itemCate->child as $itemCateChild)
                                 <li>
-                                    <a href="{{Support::show($itemCateChild,'slug')}}" title="{{Support::show($itemCateChild,'name')}}" class="block py-2 font-semibold text-[#7e7e7e] 2xl:text-[1.125rem]">{{Support::show($itemCateChild,'name')}}</a>
+                                    <a href="{{ Support::show($itemCateChild, 'slug') }}" title="{{ Support::show($itemCateChild, 'name') }}" class="block py-2 font-semibold text-[#7e7e7e] 2xl:text-[1.125rem]">{{ Support::show($itemCateChild, 'name') }}</a>
                                 </li>
                             @endforeach
                         </ul>
