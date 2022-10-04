4fe9cbc4aa55cb998dc64a382d9041ce908cc31e
RONIEJISA <roniejisa@gmail.com>
2022-10-04 10:35:01 +0700

    udpate code

--- resources/views/question_categories/all.blade.php
+++ resources/views/question_categories/all.blade.php
@@ -7,32 +7,7 @@
     </div>
     <section class="section-qa bg-[#fcfcfc] py-6 2xl:py-10">
         <div class="container">
-            @php
-                $listCate = \App\Models\QuestionCategory::act()
-                    ->where('parent', 0)
-                    ->with([
-                        'child' => function ($q) {
-                            $q->act();
-                        },
-                    ])
-                    ->get();
-            @endphp
-            <!-- <span class="show-category mb-4 inline-block cursor-pointer rounded-md bg-[#fb2317] p-2 text-white lg:hidden">Danh mục</span> -->
-            <div class="box relative">
-                <i class="fa fa-sort absolute top-1/2 right-4 -translate-y-1/2" aria-hidden="true"></i>
-                <ul class="w-full appearance-none rounded bg-[#f5f5f5] py-2 px-4">
-                    <li><a href="javascript:void(0)">Tất cả</a></li>
-                    @foreach ($listCate as $itemCate)
-                        @if (count($itemCate->child) > 0)
-                            @foreach ($itemCate->child as $itemCateChild)
-                                <li>
-                                    <a href="{{ Support::show($itemCateChild, 'slug') }}" title="{{ Support::show($itemCateChild, 'name') }}" class="block py-2 font-semibold text-[#7e7e7e] 2xl:text-[1.125rem]">{{ Support::show($itemCateChild, 'name') }}</a>
-                                </li>
-                            @endforeach
-                        @endif
-                    @endforeach
-                </ul>
-            </div>
+            @include('question_categories.select')
             <div class="grid grid-cols-1 gap-4 lg:grid-cols-4 2xl:gap-8">
                 <div class="col-span-1">
                     @include('question_categories.sidebar')
